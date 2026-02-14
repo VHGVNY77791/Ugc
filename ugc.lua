1193e088ddf626d0333867e8da045f1fe57ec268
VHGVNY77791 <chiyphrmxiththidech@gmail.com>
2026-02-15 03:19:27 +0700

    Update ugc.lua

--- ugc.lua
+++ ugc.lua
@@ -1,1 +1,30 @@
-print("UGC SCRIPT LOADED")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local UGC_ID = "1744060292"
+
+local function wear(char)
+    local hum = char:WaitForChild("Humanoid")
+
+    for i = 1,8 do
+        task.wait(1)
+
+        local ok, acc = pcall(function()
+            return game:GetObjects("rbxassetid://"..UGC_ID)[1]
+        end)
+
+        if ok and acc then
+            hum:AddAccessory(acc)
+            print("UGC WORN")
+            break
+        else
+            print("retry...", i)
+        end
+    end
+end
+
+if player.Character then
+    wear(player.Character)
+end
+
+player.CharacterAdded:Connect(wear)
